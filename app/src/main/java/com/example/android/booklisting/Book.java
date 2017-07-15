5dda8fbd66e72397bbabb7846180ba08559ed66a
Andreea Manea <andreeasky@yahoo.com>
2017-07-15 22:15:04 +0300

    Some updates to my BookListing App. :)

--- app/src/main/java/com/example/android/booklisting/Book.java
+++ app/src/main/java/com/example/android/booklisting/Book.java
@@ -1,12 +1,15 @@
 package com.example.android.booklisting;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * Created by User on 7/10/2017.
  */
 
 // A Book object contains information related to a single book.
 
-public class Book {
+public class Book implements Parcelable{
 
     // Book title
     private String bookTitle;
@@ -17,6 +20,19 @@ public class Book {
     // Book description
     private String bookDescription;
 
+    @Override
+    public void writeToParcel(Parcel out, int flags) {
+        out.writeString(bookTitle);
+        out.writeString(bookAuthor);
+        out.writeString(bookDescription);
+    }
+
+    private Book(Parcel in) {
+        bookTitle = in.readString();
+        bookAuthor = in.readString();
+        bookDescription = in.readString();
+    }
+
     /**
      * Constructs a new Book object.
      *
@@ -25,10 +41,33 @@ public class Book {
      * @param description is the description of the book
      */
     public Book(String title, String authors, String description) {
+        // Normal actions performed by class, since this is still a normal object!
         bookTitle=title;
         bookAuthor=authors;
         bookDescription=description;
     }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    public static final Parcelable.Creator<Book> CREATOR
+            = new Parcelable.Creator<Book>() {
+
+        // This simply calls our new constructor (typically private) and
+        // passes along the unmarshalled `Parcel`, and then returns the new object!
+        @Override
+        public Book createFromParcel(Parcel in) {
+            return new Book(in);
+        }
+
+        // We just need to copy this and change the type to match our class.
+        @Override
+        public Book[] newArray(int size) {
+            return new Book[size];
+        }
+    };
 
     /**
      * Returns the title of the book.
--- app/src/main/java/com/example/android/booklisting/BookActivity.java
+++ app/src/main/java/com/example/android/booklisting/BookActivity.java
@@ -1,6 +1,5 @@
 package com.example.android.booklisting;
 
-import android.app.Fragment;
 import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
@@ -27,7 +26,7 @@ public class BookActivity extends AppCompatActivity {
     public static final String LOG_TAG=BookActivity.class.getName();
 
     // This is the Google API URL
-    private static final String BASE_URL = "https://www.googleapis.com/books/v1/volumes?&maxResults=10&q=";
+    private static final String BASE_URL="https://www.googleapis.com/books/v1/volumes?&maxResults=10&q=";
 
     // Add a maximum results of 20 to search query
     private static final String MAX_RESULTS="&maxResults=20";
@@ -46,10 +45,6 @@ public class BookActivity extends AppCompatActivity {
 
     // Checker for the internet connection
     private boolean isInternetConnected;
-
-    private static final String LIST_STATE = "listState";
-
-    private Parcelable mListState = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -78,7 +73,7 @@ public class BookActivity extends AppCompatActivity {
         isInternetConnected=checkInternetConnection();
 
         // Create a new adapter that takes an empty list of books as input
-              bookAdapter=new BookAdapter(this, new ArrayList<Book>());
+        bookAdapter=new BookAdapter(this, new ArrayList<Book>());
 
         // Set the adapter on the ListView so the list can be populated in the user interface
         bookListView.setAdapter(bookAdapter);
@@ -118,148 +113,101 @@ public class BookActivity extends AppCompatActivity {
                 }
             }
         });
-        
-        // Write list state to bundle
+
+        savedInstanceState.getParcelableArrayList("book");
+    }
+
+    List<Book> books = new ArrayList<>();
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putParcelableArrayList("book", (ArrayList<? extends Parcelable>) books);
+        super.onSaveInstanceState(outState);
+    }
+
+    // Check the internet connection
+    private boolean checkInternetConnection() {
+        // Get a reference to the ConnectivityManager to check the state of network connectivity
+        ConnectivityManager connectivityManager=(ConnectivityManager)
+                getSystemService(CONNECTIVITY_SERVICE);
+
+        // Get details on the currently active default network
+        NetworkInfo networkInfo=connectivityManager.getActiveNetworkInfo();
+        if (networkInfo != null && networkInfo.isConnected()) {
+            return true;
+        } else {
+            // Show the empty state with no connection error message
+            emptyStateTextView.setVisibility(View.VISIBLE);
+            // Update empty state with no connection error message
+            emptyStateTextView.setText(R.string.no_internet_connection);
+            return false;
+        }
+    }
+
+    // {@link AsyncTask} to perform the network request on a background thread, and then
+    // update the UI with the list of earthquakes in the response.
+    //
+    // We'll only override two of the methods of AsyncTask: doInBackground() and onPostExecute().
+    // The doInBackground() method runs on a background thread, so it can run long-running code
+    // (like network activity), without interfering with the responsiveness of the app.
+    // Then onPostExecute() is passed the result of doInBackground() method, but runs on the
+    // UI thread, so it can use the produced data to update the UI.
+    //
+    private class BookAsyncTask extends AsyncTask<String, Void, List<Book>> {
+
+        // This method runs on the UI thread before foInBackground().
+        // It shows the progress bar when the internet connection is delayed or slow.
+        //
         @Override
-        protected void onSaveInstanceState (Bundle state){
-            super.onSaveInstanceState(state);
-            mListState=getbookListView().onSaveInstanceState();
-            state.putParcelable(LIST_STATE, mListState);
+        protected void onPreExecute() {
+            Log.e(LOG_TAG, "When is the onPreExecute called?");
+            // Show loading indicator if the internet connection is delayed or slow.
+            loadingIndicator.setVisibility(View.VISIBLE);
         }
 
-    private Fragment getbookListView() {
-        return null;
-    }
+        // This method runs on a background thread and performs the network request.
+        // We should not update the UI from a background thread, so we return a list of
+        // Books as the result.
+        //
+        @Override
+        protected List<Book> doInBackground(String... urls) {
+            // Don't perform the request if there are no URLs, or the first URL is null.
+            if (urls.length < 1 || urls[0] == null) {
+                return null;
+            }
+            List<Book> result=Utils.fetchBooksData(urls[0]);
+            Log.i("search", urls[0]);
 
-    // Create a new adapter that takes an empty list of books as input
-
-    bookAdapter=new BookAdapter(this, new ArrayList<Book>());
-
-    // Set the adapter on the ListView so the list can be populated in the user interface
-        bookListView.setAdapter(bookAdapter);
-
-    @Override
-    public void onPause() {
-        // Save ListView state @ onPause
-        Log.d(TAG, "saving listview state @ onPause");
-        state=bookListView.onSaveInstanceState();
-        super.onPause();
-    }
-
-    @Override
-    public void onViewCreated(final View view, Bundle onsaveInstanceState) {
-        super.onViewCreated(view, onsaveInstanceState);
-        // Set new items - Set the adapter on the ListView so the list can be populated in the user interface
-        bookListView.setAdapter(bookAdapter);
-        // Restore previous state (including selected item index and scroll position)
-        if (state != null) {
-            Log.d(TAG, "trying to restore listview state..");
-            listView.onRestoreInstanceState(state);
+            return result;
         }
-    }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        loadData(); // make sure data has been reloaded into adapter first
-        // ONLY call this part once the data items have been loaded back into the adapter
-        // for example, inside a success callback from the network
-        if (mListState != null) {
-            bookListView.onRestoreInstanceState(mListState);
-            mListState=null;
-        }
-    }
+        // This method runs on the main UI thread after the background work has been
+        // completed. This method receives as input, the return value from the doInBackground()
+        // method. First we clear out the adapter, to get rid of books data from a previous
+        // query to Google Books API. Then we update the adapter with the new list of books,
+        // which will trigger the ListView to re-populate its list items.
+        //
+        @Override
+        protected void onPostExecute(List<Book> books) {
+            // First, hide loading indicator so error will be visible
+            loadingIndicator.setVisibility(View.GONE);
+            // Clear the adapter of previous book data
+            bookAdapter.clear();
 
-    // Restore list state from bundle
-    @Override
-    protected void onRestoreInstanceState(Bundle state) {
-        super.onRestoreInstanceState(state);
-        mListState=state.getParcelable(LIST_STATE);
-        {
-
-            // Check the internet connection
-            private boolean checkInternetConnection (){
-            // Get a reference to the ConnectivityManager to check the state of network connectivity
-            ConnectivityManager connectivityManager=(ConnectivityManager)
-                    getSystemService(CONNECTIVITY_SERVICE);
-
-            // Get details on the currently active default network
-            NetworkInfo networkInfo=connectivityManager.getActiveNetworkInfo();
-            if (networkInfo != null && networkInfo.isConnected()) {
-                return true;
+            // If there is a valid list of Books, then add them to the adapter's
+            // data set. This will trigger the ListView to update.
+            if (books != null && !books.isEmpty()) {
+                bookAdapter.addAll(books);
             } else {
                 // Show the empty state with no connection error message
                 emptyStateTextView.setVisibility(View.VISIBLE);
                 // Update empty state with no connection error message
-                emptyStateTextView.setText(R.string.no_internet_connection);
-                return false;
-            }
-        }
-
-            // {@link AsyncTask} to perform the network request on a background thread, and then
-            // update the UI with the list of earthquakes in the response.
-            //
-            // We'll only override two of the methods of AsyncTask: doInBackground() and onPostExecute().
-            // The doInBackground() method runs on a background thread, so it can run long-running code
-            // (like network activity), without interfering with the responsiveness of the app.
-            // Then onPostExecute() is passed the result of doInBackground() method, but runs on the
-            // UI thread, so it can use the produced data to update the UI.
-            //
-            private class BookAsyncTask extends AsyncTask<String, Void, List<Book>> {
-
-                // This method runs on the UI thread before foInBackground().
-                // It shows the progress bar when the internet connection is delayed or slow.
-                //
-                @Override
-                protected void onPreExecute() {
-                    Log.e(LOG_TAG, "When is the onPreExecute called?");
-                    // Show loading indicator if the internet connection is delayed or slow.
-                    loadingIndicator.setVisibility(View.VISIBLE);
-                }
-
-                // This method runs on a background thread and performs the network request.
-                // We should not update the UI from a background thread, so we return a list of
-                // Books as the result.
-                //
-                @Override
-                protected List<Book> doInBackground(String... urls) {
-                    // Don't perform the request if there are no URLs, or the first URL is null.
-                    if (urls.length < 1 || urls[0] == null) {
-                        return null;
-                    }
-                    List<Book> result=Utils.fetchBooksData(urls[0]);
-                    Log.i("search", urls[0]);
-
-                    return result;
-                }
-
-                // This method runs on the main UI thread after the background work has been
-                // completed. This method receives as input, the return value from the doInBackground()
-                // method. First we clear out the adapter, to get rid of books data from a previous
-                // query to Google Books API. Then we update the adapter with the new list of books,
-                // which will trigger the ListView to re-populate its list items.
-                //
-                @Override
-                protected void onPostExecute(List<Book> books) {
-                    // First, hide loading indicator so error will be visible
-                    loadingIndicator.setVisibility(View.GONE);
-                    // Clear the adapter of previous book data
-                    bookAdapter.clear();
-
-                    // If there is a valid list of Books, then add them to the adapter's
-                    // data set. This will trigger the ListView to update.
-                    if (books != null && !books.isEmpty()) {
-                        bookAdapter.addAll(books);
-                    } else {
-                        // Show the empty state with no connection error message
-                        emptyStateTextView.setVisibility(View.VISIBLE);
-                        // Update empty state with no connection error message
-                        emptyStateTextView.setText(R.string.no_data);
-                    }
-                }
+                emptyStateTextView.setText(R.string.no_data);
             }
         }
     }
 }
+
+
 
 
